1c9310455e5bddcaabc6f430d576e27b7d68f5df
Brandon Williams <brando5393@gmail.com>
2020-12-11 16:23:54 -0600

    created ui for login page

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,11 +1,44 @@
 import React from "react";
+import JumboHeading from "../../components/JumboHeading/JumboHeading";
 
-const Login = () => {
+const Login = (props) => {
   return (
     <div>
-      Login Cupiditate ab magni labore placeat itaque. Debitis inventore quo
-      molestiae aut nemo maiores eos temporibus rerum. Consectetur ut eveniet
-      recusandae sunt veniam repellendus saepe.
+      <JumboHeading mainText="Login" subText="Welcome back" />
+      <form method="POST" action="/login">
+        <label for="emailInput" className="form-label">
+          E-mail:
+        </label>
+        <input
+          type="email"
+          id="emailInput"
+          name="emailInput"
+          className="form-control"
+          placeholder="Jdoe1123@gmail.com"
+          aria-describedby="emailInputHelp"
+          required
+        />
+        <span id="emailInputHelp" class="form-text">
+          Please enter your e-mail.
+        </span>
+        <label for="passwordInput" className="form-label">
+          Password:
+        </label>
+        <input
+          type="password"
+          id="passwordInput"
+          name="passwordInput"
+          className="form-control"
+          aria-describedby="passwordInputHelp"
+          required
+        />
+        <span id="passwordInputHelp" class="form-text">
+          Please enter your password.
+        </span>
+        <button type="submit" id="loginSubmit" className="btn btn-success">
+          Login
+        </button>
+      </form>
     </div>
   );
 };
